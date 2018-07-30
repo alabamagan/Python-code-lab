fa7dc7f7864e5fc547aa6ed2b8c747adf8682509
Matthew Wong <fromosia@gmail.com>
2018-07-30 16:36:27 +0800

    Add some intuitive explainations

--- FourierPolarTransform/AliasingArtefacts.py
+++ FourierPolarTransform/AliasingArtefacts.py
@@ -2,25 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-# x = np.linspace(0, 1024/60., 1024)
-# y = 40*np.sin(60*np.pi*x)+150*np.sin(25*np.pi*x)+15*np.sin(18*np.pi*x)+240*np.sin(np.pi*x)
-# fy = np.fft.fft(y)
-#
-# fx = np.linspace(0, len(x)/(x[-1] - x[0]), len(x)//2)                   # Frequency range
-# plt.plot(fx, (np.abs(fy[:len(fy)/2]))*2./len(x))
-#
-# # fx = np.linspace(0, len(x)*2/(x[-1]-x[0]), len(x))
-# # plt.plot(fx, np.abs(fy))
-# plt.show()
+N = 1024.       # Sampled points
+s = 64.         # Sampling rate
+R = N/s         # x-space range
 
-
-x = np.linspace(-10, 10, 1024*5)
-B = 80
-y = B * np.sin(2*np.pi*B*x)/(np.pi * x) - (np.sin(np.pi * B * x)/(np.pi * x))**2
+x = np.linspace(0, R, N)    # x-space steps
+y = 40*np.sin(60*np.pi*x)+150*np.sin(25*np.pi*x)+15*np.sin(18*np.pi*x)  # Signal
 fy = np.fft.fft(y)
 
-plt.plot(x, np.abs(np.fft.fftshift(fy)))
-# plt.plot(x, y)
+fx = np.linspace(0, s, len(x)//2)                   # Frequency range
+plt.plot(fx, (np.abs(fy[:len(fy)/2]))*2./N)         # Integration is normalize by dividing half of number of points
+
 plt.show()
+
 
 
