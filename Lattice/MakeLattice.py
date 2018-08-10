66458f96d5a1f7c2ef93546cdfafd5cbace8650a
Matthew Wong <fromosia@gmail.com>
2018-08-10 18:14:56 +0800

    Add some visualization functions

--- Lattice/MakeLattice.py
+++ Lattice/MakeLattice.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from Algorithms.FilterBanks.FilterBanks import Downsample
+from Algorithms.FilterBanks.FilterBanks import Downsample, Upsample
 
 class Lattice2D(object):
     def __init__(self):
@@ -19,6 +19,51 @@ class Lattice2D(object):
 
         return np.array(lattice)
 
+def plotTransformQuiver(d, F=5):
+    fig, ax = plt.subplots(1, 1)
+    X = np.array(zip(d._uv[:,:,0].flatten(), d._uv[:,:,1].flatten()))
+    XYX = np.array(zip(d._omega[0][:,:,0].flatten(), d._omega[0][:,:,1].flatten()))
+    XYX2 = np.array(zip(d._omega[1][:,:,0].flatten(), d._omega[1][:,:,1].flatten()))
+
+    ax.scatter(X[:,0], X[:,1])
+    ax.scatter(XYX[:,0], XYX[:,1], s=5)
+    ax.quiver(d._uv[:,:,0].flatten()[::F], d._uv[:,:,1].flatten()[::F],
+              d._omega[0][:,:,0].flatten()[::F] - d._uv[:,:,0].flatten()[::F],
+              d._omega[0][:,:,1].flatten()[::F] - d._uv[:,:,1].flatten()[::F],
+              scale=1, scale_units='xy', alpha=0.5, width=0.002)
+    ax.spines['left'].set_position('center')
+    ax.spines['right'].set_color('none')
+    ax.spines['bottom'].set_position('center')
+    ax.spines['top'].set_color('none')
+    ax.spines['left'].set_smart_bounds(True)
+    ax.spines['bottom'].set_smart_bounds(True)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    plt.axis('equal')
+    plt.show()
+
+def plotScatter(d):
+
+    fig, ax = plt.subplots(1, 1)
+    X = np.array(zip(d._uv[:,:,0].flatten(), d._uv[:,:,1].flatten()))
+    XYX = np.array(zip(d._omega[0][:,:,0].flatten(), d._omega[0][:,:,1].flatten()))
+    XYX2 = np.array(zip(d._omega[1][:,:,0].flatten(), d._omega[1][:,:,1].flatten()))
+
+    ax.scatter(X[:,0], X[:,1])
+    ax.scatter(XYX[:,0], XYX[:,1], s=5)
+    ax.scatter(XYX2[:,0], XYX2[:,1], s=5, c='r')
+    ax.spines['left'].set_position('center')
+    ax.spines['right'].set_color('none')
+    ax.spines['bottom'].set_position('center')
+    ax.spines['top'].set_color('none')
+    ax.spines['left'].set_smart_bounds(True)
+    ax.spines['bottom'].set_smart_bounds(True)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    plt.axis('equal')
+    plt.show()
 
 if __name__ == '__main__':
     B1 = np.array([[1,0],[0,1]])
@@ -32,38 +77,12 @@ if __name__ == '__main__':
     test._basis = np.array(B2)
     lattice2 = test.sample_lattice(3)
 
-    d = Downsample()
-    out = d.run(np.random.random([16,16]))
+    d = Upsample()
+    out = d.run(np.random.random([8,8, 2]))
 
 
-    # x, y = np.meshgrid(np.arange(5) - 2, np.arange(5) - 2)
-    # XY = np.stack([x, y])
-    # angle = np.deg2rad(30)
-    # M = np.array([[np.cos(angle), -np.sin(angle)],
-    #               [np.sin(angle), np.cos(angle)]])
-    # XY = M.dot(XY)
-    # X = np.array(zip(x.flatten(), y.flatten()))
-    # XYX = np.array(zip(XY[:,:,0].flatten(), XY[:,:,1].flatten()))
-
-    X = np.array(zip(d._uv[:,:,0].flatten(), d._uv[:,:,1].flatten()))
-    XYX = np.array(zip(d._omega[0][:,:,0].flatten(), d._omega[0][:,:,1].flatten()))
-
-    fig, ax = plt.subplots(1, 1)
-    # ax.scatter(lattice1[:,0], lattice1[:,1])
-    # ax.scatter(lattice2[:,0]+1, lattice2[:,1], s=4)
-    ax.scatter(X[:,0], X[:,1])
-    ax.scatter(XYX[:,0], XYX[:,1], s=5)
-    ax.spines['left'].set_position('center')
-    ax.spines['right'].set_color('none')
-    ax.spines['bottom'].set_position('center')
-    ax.spines['top'].set_color('none')
-    ax.spines['left'].set_smart_bounds(True)
-    ax.spines['bottom'].set_smart_bounds(True)
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
-
-    plt.axis('equal')
-    plt.show()
+    plotTransformQuiver(d, 1)
+    # plotScatter(d)
 
 
 
